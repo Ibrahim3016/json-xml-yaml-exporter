153a0a398df4ee0a5bcf97b7e76b24c5865116cf
Ibrahim Adeyemi <ibrahimadeyemi88@gmail.com>
2020-04-21 17:24:05 +0100

    Go task 3

---
+++ exporter.go
@@ -0,0 +1,81 @@
+package exporter
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"os"
+)
+
+// SocialMedia is  an interface of feeds
+type SocialMedia interface {
+	Feed() []string
+}
+
+// TextFile converts SocialMedia feeds to txt file
+func TextFile(u SocialMedia, filename string) error {
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		return errors.New("an error occured opening the file: " + err.Error())
+	}
+	for _, fd := range u.Feed() {
+		n, err := f.Write([]byte(fd + "\n"))
+		if err != nil {
+			return errors.New("an error occured writing to file: " + err.Error())
+		}
+		fmt.Printf("wrote %d bytes\n", n)
+	}
+	return nil
+}
+
+// JSONFile converts SocialMedia feeds to json file
+func JSONFile(u SocialMedia, filename string) error {
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0755)
+	defer f.Close()
+
+	if err != nil {
+		return errors.New("an error occured opening the file: " + err.Error())
+	}
+
+	for _, dd := range u.Feed() {
+		b, err := json.MarshalIndent(dd, "\n", "\n")
+		if err != nil {
+			return errors.New("an error occured writing to file: " + err.Error())
+		}
+
+		bytesWritten, err := f.Write(b)
+
+		if err != nil {
+			return errors.New("an error occured writing to file: " + err.Error())
+		}
+		fmt.Printf("wrote %d bytes\n", bytesWritten)
+	}
+
+	return nil
+}
+
+// XMLFile converts SocialMedia feeds to XML file
+func XMLFile(u SocialMedia, filename string) error {
+	x, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0755)
+	defer x.Close()
+
+	if err != nil {
+		return errors.New("an error occured opening the file: " + err.Error())
+	}
+
+	for _, ld := range u.Feed() {
+		f, err := xml.MarshalIndent(ld, "\n", "\n")
+		if err != nil {
+			return errors.New("an error occured writing to file: " + err.Error())
+		}
+
+		bytesWritten, err := x.Write(f)
+
+		if err != nil {
+			return errors.New("an error occured writing to file: " + err.Error())
+		}
+		fmt.Printf("wrote %d bytes\n", bytesWritten)
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module week3
 
 go 1.14
-
-require gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
